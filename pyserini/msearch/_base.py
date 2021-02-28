ca7e1d8b0b12b3e13173a8c265c123c5131c3c16
t-k- <clock126@126.com>
2021-03-01 02:58:55 +0800

    msearch: add support to json math topics files, and update _topic_process_ handlers.

--- pyserini/msearch/_base.py
+++ pyserini/msearch/_base.py
@@ -1,25 +1,62 @@
 import os
+import json
 import subprocess
 
 from pyserini.util import get_cache_home
 from pyserini.util import download_evaluation_script
 
-def _line_process__ntcir12_math_browsing(line):
+
+def _topic_process__ntcir12_math_browsing(line):
     fields = line.split()
     query = [{'type': 'tex', 'keyword': fields[1]}]
     qid = fields[0]
     return qid, query
 
 
+def _topic_process__ntcir12_math_browsing_concrete(line):
+    fields = line.split()
+    query = [{'type': 'tex', 'keyword': fields[1]}]
+    qid = fields[0]
+    return qid, query
+
+
+def _topic_process__ntcir12_math_browsing_wildcards(line):
+    fields = line.split()
+    query = [{'type': 'tex', 'keyword': fields[1]}]
+    qid = fields[0]
+    return qid, query
+
+
+def _topic_process__arqmath_2020_task1(json_item):
+    query = [{'type': kw['type'], 'keyword': kw['str']} for kw in json_item['kw']]
+    qid = 'A.' + str(json_item['qid'])
+    return qid, query
+
+
 def gen_topics_queries(collection: str):
+    func_name = '_topic_process__' + collection.replace('-', '_')
+    handler = globals()[func_name] if func_name in globals() else None
     cache = get_cache_home()
-    src = f'./tools/topics-and-qrels/topics.{collection}.txt'
-    with open(src, 'r') as fh:
-        for line in fh:
-            line = line.rstrip()
-            func_name = '_line_process__' + collection.replace('-', '_')
-            handler = globals()[func_name]
-            yield handler(line)
+    prefix = f'./tools/topics-and-qrels/topics.{collection}'
+    found = False
+    for src in [f'{prefix}.{ent}' for ent in ['txt', 'json']]:
+        if not os.path.exists(src):
+            continue
+        else:
+            found = True
+        ext = src.split('.')[-1]
+        if ext == 'txt':
+            with open(src, 'r') as fh:
+                for line in fh:
+                    line = line.rstrip()
+                    yield handler(line)
+        elif ext == 'json':
+            with open(src, 'r') as fh:
+                qlist = json.load(fh)
+                for json_item in qlist:
+                    yield handler(json_item)
+    if not found:
+        raise ValueError(f'Unrecognized index name {collection}')
 
 
 def get_qrels_filepath(collection: str):
