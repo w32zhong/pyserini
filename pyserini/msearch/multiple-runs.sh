0a230622a44060cb4fc502fbaf7e5e775dc949fe
t-k- <clock126@126.com>
2021-03-05 16:29:54 +0000

    added pyserini/msearch/multiple-runs.sh

---
+++ pyserini/msearch/multiple-runs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+NAME=arqmath-2020-task1
+
+INDEX=$NAME
+TOPICS=$NAME
+RUN=$NAME
+
+single_run() {
+	num=${1-1}
+	python3 -m pyserini.msearch --index $INDEX --topk 1000 --eval-math-topics $TOPICS --trec-output ./runs/$RUN.run --verbose > ./runs/$RUN.$num.output
+	runtimes=$(cat ./runs/$RUN.$num.output | ./pyserini/msearch/collect-runtimes.sh)
+	python3 ./pyserini/msearch/calc-runtime-stats.py $runtimes > ./runs/$RUN.$num.runtimes
+}
+
+for i in {1..5}; do
+	single_run $i
+done
